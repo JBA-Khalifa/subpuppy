9b0731fba1303551f7f040955dbc171bc6fd7429
chiaforce <754800643@qq.com>
2021-11-02 19:26:34 +0800

    0.1.6

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import { logger } from "./logger";
 import { program } from "commander";
 import { exit } from "process";
 import { fetchChainData, getDBHeight } from "./controller/services/db";
+import { Extrinsics } from "./entity/Extrinsics";
 
 let updateAllowed = false;
 let fetchingData = false;
@@ -98,6 +99,25 @@ program
 
             app.listen(parseInt(options.port));
             logger.info(`Express server has started on port ${options.port}.`);
+        }).catch(error => console.log(error));
+    })
+
+program
+    .command('dev [options]')
+    .description('Repare data just for development')
+    .option('-f --from <blockHeight>', 'from block height')
+    .option('-t --to <blockHeight>', 'to block height')
+    .action((name, options, command) => {
+        createConnection().then(async (conn) => {
+            const sql = `select * from extrinsics where block_num >= ${options.from} and block_num <= ${options.to}`;
+            const result: Array<Extrinsics> = await conn.manager.query(sql);
+            for(let i = 0; i < result.length; i++) {
+                const sql2 = `update blocks set block_timestamp = ${result[i].block_timestamp} where block_num = ${result[i].block_num} and block_timestamp > 2000000000`;
+                const re = await conn.manager.query(sql2);
+                if(re.changedRows > 0) console.log(`Update #${result[i].block_num} timestamp to ${result[i].block_timestamp}`);
+            }
+            console.log("Done...");
+            exit();
         }).catch(error => console.log(error));
     })
 
