452db349142a440a77a9c4427043980113018790
chiaforce <754800643@qq.com>
2021-11-16 12:02:12 +0800

    mainnet launch

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
-export const debug = true;
-export const wss = 'wss://protonet.xxlabs.net';
+export const debug = false;
+export const wss = 'wss://mainnet.xxlabs.net';
+// export const wss = 'wss://protonet.xxlabs.net';
 // export const wss = 'wss://kusama-rpc.polkadot.io';
